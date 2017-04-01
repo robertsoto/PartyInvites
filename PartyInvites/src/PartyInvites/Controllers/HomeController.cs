ca0061483804d1f920b56bf358249c3ba161e708
Rob Soto <rob.soto@live.com>
2017-04-01 14:41:37 -0400

    Finished Chapter 2

--- PartyInvites/src/PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/src/PartyInvites/Controllers/HomeController.cs
@@ -3,33 +3,67 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
 
 namespace PartyInvites.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
-        {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        //public IActionResult About()
+        //{
+        //    ViewData["Message"] = "Your application description page.";
+
+        //    return View();
+        //}
+
+        //public IActionResult Contact()
+        //{
+        //    ViewData["Message"] = "Your contact page.";
+
+        //    return View();
+        //}
+
+        //public IActionResult Error()
+        //{
+        //    return View();
+        //}
+
+        public ViewResult Index() {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View("MyView");
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm() {
             return View();
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
 
-            return View();
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                // there is a validation error
+                return View();
+            }
         }
 
-        public IActionResult Contact()
+        public ViewResult ListResponses()
         {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
         }
 
-        public IActionResult Error()
-        {
-            return View();
-        }
     }
 }
---
+++ PartyInvites/src/PartyInvites/Models/Repository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartyInvites.Models
+{
+    public static class Repository
+    {
+        private static List<GuestResponse> responses = new List<GuestResponse>();
+
+        public static IEnumerable<GuestResponse> Responses
+        {
+            get { return responses; }
+        }
+
+        public static void AddResponse (GuestResponse response)
+        {
+            responses.Add(response);
+        }
+
+
+//        Page 33 -- The Repository class and its members are static , which will make it easy for me 
+//        to store and retrieve data from different places in the application.
+//        MVC provides a more sophisticated approach for defining
+//        common functionality, called dependency injection , which I describe in Chapter 18 , 
+//        but a static class is a good way to get started for a simple application like this one.
+
+    }
+}
